87f7dd22397a611962ad03f89bb7f8a8dbf10c0c
Bruno Chaves <chavesbruno699@gmail.com>
2024-04-30 14:44:57 -0300

    server:HTTP Methods

--- 01-fundamentals/src/server.js
+++ 01-fundamentals/src/server.js
@@ -1,6 +1,19 @@
 import http from 'node:http';
 
+// GET /users => get users on the dataBase
+// POST /users => Create new User in the dataBase
+
 const server = http.createServer((req, res) => {
+    const { method, url } = req;
+
+    if(method === "GET" && url === "/users"){
+        return res.end("User list here!")
+    }
+
+    if(method === "POST" && url === "/users"){
+        return res.end("User has been created!")
+    }
+
     return res.end("Hello World!")
 })
 
