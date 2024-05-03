6efe555b3e3147a3798b42c4f64937158568eda1
Bruno Chaves <chavesbruno699@gmail.com>
2024-05-02 23:50:57 -0300

    stream: _writableStream && _transformStream

--- 01-fundamentals/src/server.js
+++ 01-fundamentals/src/server.js
@@ -1,18 +1,5 @@
 import http from 'node:http';
 
-/**
- * Stateless API:
- * - Save data in databases or otherPlaces
- * - In a stateless API, the server does not store any client session information between requests. 
- *   Each request from the client is independent and self-contained.
- * 
- * StateFul API: 
- * - Save data in webSession and restore the data each initialization
- * - Stateful APIs are often used in scenarios where maintaining session state across multiple requests 
- *   is necessary, such as in traditional web applications with user sessions.
- * 
- */
-
 const users = [];
 
 const server = http.createServer((req, res) => {
--- 01-fundamentals/streams/fundamentals.js
+++ 01-fundamentals/streams/fundamentals.js
@@ -1,6 +1,4 @@
-import { Readable } from 'node:stream';
-
-// process.stdin.pipe(process.stdout);
+import { Readable, Writable, Transform } from 'node:stream';
 
 class OneToHundredStream extends Readable {
     index = 1
@@ -16,8 +14,25 @@ class OneToHundredStream extends Readable {
 
                 this.push(buf);
             }
-        }, 500)
+        }, 1000)
     }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class InverNumberStream extends Transform{
+    _transform(chunk, encoding, callback){
+        const transformed = Number(chunk.toString()) * -1;
+
+        callback(null, Buffer.from(String(transformed)));
+    }
+}
+
+class MultiplyByTenStream extends Writable{
+    _write(chunk, encoding, callback){
+        console.log(Number(chunk.toString()) * 10);
+        callback()
+    }
+}
+
+new OneToHundredStream()
+    .pipe(new InverNumberStream())
+    .pipe(new MultiplyByTenStream())
